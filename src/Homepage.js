3e8cee3868219a62b9748f75099314ad969a4ba3
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-11-28 18:04:11 -0300

    Add basic styles

--- src/Homepage.js
+++ src/Homepage.js
@@ -2,7 +2,7 @@ import food from './images/food.png';
 import PageWrapper from './helper-components/PageWrapper';
 import SideDisplay from './helper-components/SideDisplay';
 
-const HeroTitle = () => ({ type: 'h1', text: 'Well prepared, tasteful vegan food with everything you could wish!'});
+const HeroTitle = () => ({ type: 'h1', text: 'Well prepared, tasteful vegetarian and vegan food with everything you could wish!'});
 const HeroParagraph = () => ({ text: 'You don\'t need to eat tasteless food, this world can be very colorful and tasteful. Vegetarian food like you will never see anywhere else. You will not even notice the difference, come and visit us!' });
 const HeroText = () => ({ type: 'div', children: [HeroTitle(), HeroParagraph()], attr: { class: 'hero-text' } })
 const HeroImage = () => ({ type: 'div', children: [{ type: 'img', attr: { src: food }}]});
